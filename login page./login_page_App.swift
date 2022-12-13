45f4889104f2db23decb6704bc99c68b54c41f42
Raniyah44 <rania.alotaibi@hotmail.com>
2022-12-13 12:02:10 +0300

    Initial Commit

---
+++ login page./login_page_App.swift
@@ -0,0 +1,17 @@
+//
+//  login_page_App.swift
+//  login page.
+//
+//  Created by Raniyah Alotaibi on 19/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct login_page_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
